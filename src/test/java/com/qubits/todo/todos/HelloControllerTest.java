71211fcfd66acd6e21d5bdace24a8d9175ffa4da
Mustafa Mahrous <m.qamaruddin.89@gmail.com>
2023-07-02 19:57:14 +0200

    Add hello controller and test: remove empty line

--- src/test/java/com/qubits/todo/todos/HelloControllerTest.java
+++ src/test/java/com/qubits/todo/todos/HelloControllerTest.java
@@ -18,8 +18,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-@AutoConfigureMockMvc(addFilters = false
-)
+@AutoConfigureMockMvc(addFilters = false)
 class HelloControllerTest {
 
   @Autowired
