4be29b9b28c899bedb78a5b8e20f4bde83eb5446
Mustafa Mahrous <m.qamaruddin.89@gmail.com>
2023-07-02 19:56:22 +0200

    Add hello controller and test: disable security filters in unit tests

--- src/test/java/com/qubits/todo/todos/HelloControllerTest.java
+++ src/test/java/com/qubits/todo/todos/HelloControllerTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
@@ -16,7 +18,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-@AutoConfigureMockMvc
+@AutoConfigureMockMvc(addFilters = false
+)
 class HelloControllerTest {
 
   @Autowired
